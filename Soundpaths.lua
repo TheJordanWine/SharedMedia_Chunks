e0561ed75921e7c2f8de837742a542daa4474ff8
Jordan Wine <jordandwine@gmail.com>
2023-01-28 18:20:01 -0800

    initial commit

---
+++ Soundpaths.lua
@@ -0,0 +1,30 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFF5156EDbite|r", [[Interface\Addons\SharedMedia_Chunks\sound\bite.ogg]])
+LSM:Register("sound", "|cFF5156EDkick|r", [[Interface\Addons\SharedMedia_Chunks\sound\kick.ogg]])
+LSM:Register("sound", "|cFF5156EDmangle|r", [[Interface\Addons\SharedMedia_Chunks\sound\mangle.ogg]])
+LSM:Register("sound", "|cFF5156EDstop|r", [[Interface\Addons\SharedMedia_Chunks\sound\stop.ogg]])
+LSM:Register("sound", "|cFF5156EDswarm|r", [[Interface\Addons\SharedMedia_Chunks\sound\swarm.ogg]])
+LSM:Register("sound", "|cFF5156EDkite|r", [[Interface\Addons\SharedMedia_Chunks\sound\kite.ogg]])
+LSM:Register("sound", "|cFF5156EDtrinket|r", [[Interface\Addons\SharedMedia_Chunks\sound\trinket.ogg]])
+LSM:Register("sound", "|cFF5156EDpotion|r", [[Interface\Addons\SharedMedia_Chunks\sound\potion.ogg]])
+LSM:Register("sound", "|cFF5156EDmoonfire|r", [[Interface\Addons\SharedMedia_Chunks\sound\moonfire.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
